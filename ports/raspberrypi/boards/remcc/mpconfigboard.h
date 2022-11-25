390ae57b6fce9f2374d5accb4d3591c5ddecf462
Matthew Haahr <matthew.haahr@gmail.com>
2022-11-25 08:34:56 -0500

    First Build of REMCC CircuitPMCC Circuit Python Enough for REPL Test

---
+++ ports/raspberrypi/boards/remcc/mpconfigboard.h
@@ -0,0 +1,16 @@
+#define MICROPY_HW_BOARD_NAME "REMCC sensorboard"
+#define MICROPY_HW_MCU_NAME "rp2040"
+
+#define BOARD_HAS_CRYSTAL 1
+
+#define MICROPY_HW_NEOPIXEL (&pin_GPIO17)
+
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO12)
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO13)
+
+#define DEFAULT_UART_BUS_TX (&pin_GPIO0)
+#define DEFAULT_UART_BUS_RX (&pin_GPIO1)
+
+#define DEFAULT_SPI_BUS_SCK (&pin_GPIO18)
+#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO19)
+#define DEFAULT_SPI_BUS_MISO (&pin_GPIO20)
