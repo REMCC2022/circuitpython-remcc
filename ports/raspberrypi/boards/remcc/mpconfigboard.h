02782ecc29fbd44d706de431ff58d3f2472f585f
Matthew Haahr <matthew.haahr@gmail.com>
2023-01-29 13:17:37 -0500

    Update REMCC Board Config

--- ports/raspberrypi/boards/remcc/mpconfigboard.h
+++ ports/raspberrypi/boards/remcc/mpconfigboard.h
@@ -1,12 +1,10 @@
-#define MICROPY_HW_BOARD_NAME "REMCC sensorboard"
+#define MICROPY_HW_BOARD_NAME "REMCC Sensorboard"
 #define MICROPY_HW_MCU_NAME "rp2040"
 
 #define BOARD_HAS_CRYSTAL 1
 
-#define MICROPY_HW_NEOPIXEL (&pin_GPIO17)
-
-#define DEFAULT_I2C_BUS_SDA (&pin_GPIO12)
-#define DEFAULT_I2C_BUS_SCL (&pin_GPIO13)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO8)
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO9)
 
 #define DEFAULT_UART_BUS_TX (&pin_GPIO0)
 #define DEFAULT_UART_BUS_RX (&pin_GPIO1)
